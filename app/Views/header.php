70ff5569a23093f8f139b8aa2e57f72a6801f2a2
jol-tm <joaolucastmagalhes@gmail.com>
2025-02-25 17:27:28 -0300

    Header and nav

---
+++ app/Views/header.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="<?= base_url('style.css'); ?>">
+    <title>H.E.</title>
+</head>
+<body>
+    <header id="top">
+        <a href="" class="linkHeader"><h5>CONTATOS</h5></a>
+        <a href="" class="linkHeader"><h1 id="logo">H.E.</h1></a>
+        <div id="loginAndBagWrapper">
+            <h5 class="linkHeader">ENTRAR</h5>
+            <a href="" class="linkHeader">
+                <div class="bagWrapper">
+                    <svg width="19" height="20" viewBox="0 0 19 20" fill="none" xmlns="http://www.w3.org/2000/svg"><path class="bagPath" d="M5.5 8.5C5.5 2.5 6.26293 0.5 9.5 0.5C12.7371 0.5 13.5 2.5 13.5 8.5M9.5 5H1.5L1 19H9.5H18L17.5 5H9.5Z" stroke="black" stroke-linejoin="round"/></svg>
+                    <div class="bagAmount">(0)</div>
+                </div>
+            </a>
+        </div>
+    </header>
+    <nav>
+        <div id="navOptions">
+            <a href="<?= base_url('/'); ?>" class="linkHeader"><h5>INÍCIO</h5></a>
+            <a href="" class="linkHeader"><h5>SHOP</h5></a>
+            <a href="" class="linkHeader"><h5>CONTATOS</h5></a>
+            <h5 id="">ENTRAR</h5>
+            <a href="" class="linkHeader">
+                <div class="bagWrapper">
+                    <svg width="19" height="20" viewBox="0 0 19 20" fill="none" xmlns="http://www.w3.org/2000/svg"><path class="bagPath" d="M5.5 8.5C5.5 2.5 6.26293 0.5 9.5 0.5C12.7371 0.5 13.5 2.5 13.5 8.5M9.5 5H1.5L1 19H9.5H18L17.5 5H9.5Z" stroke="black" stroke-linejoin="round"/></svg>
+                    <div class="bagAmount">(0)</div>
+                </div>
+            </a>
+        </div>
+        <a href="<?= base_url('/'); ?>" class="linkHeader"><h2 id="miniLogo">H.E.</h3></a>
+    </nav>
