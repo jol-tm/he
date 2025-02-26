46373c5b2b786bf12c5949d425b740b0dbaae9f5
jol-tm <joaolucastmagalhaes@gmail.com>
2025-02-25 22:45:48 -0300

    Footer

--- app/Views/footer.php
+++ app/Views/footer.php
@@ -1,2 +1,22 @@
+    <footer id="contacts">
+        <div id="footerContentBox">
+            <div class="footerDiv">
+                <h2>Contato & Informações</h2>
+                <a href="mailto:h.e.faleconosco@gmail.com" class="footerLink">h.e.faleconosco@gmail.com</a>
+                <span class="footerLink">(19) 98155 5749</span>
+                <a href="https://api.whatsapp.com/send/?phone=5519981555749&text&type=phone_number&app_absent=0" class="footerLink">WhatsApp</a>
+                <a href="https://www.instagram.com/h.e.loja/" class="footerLink">Instagram</a>
+            </div>
+            <div class="footerDiv">
+                <a href="<?= base_url('inDevelopment'); ?>" class="footerLink">Envios</a>
+                <a href="<?= base_url('inDevelopment'); ?>" class="footerLink">Trocas e Devoluções</a>
+                <a href="<?= base_url('inDevelopment'); ?>" class="footerLink">Termos e Condições</a>
+                <a href="<?= base_url('inDevelopment'); ?>" class="footerLink">Política de Privacidade</a>
+            </div>
+        </div>
+        <div id="footerLogo">
+            <h2>H.E.</h2><p>© 2025</p>
+        </div>
+    </footer>
 </body>
 </html>
--- app/Views/header.php
+++ app/Views/header.php
@@ -8,30 +8,30 @@
 </head>
 <body>
     <header id="top">
-        <a href="" class="linkHeader"><h5>CONTATOS</h5></a>
-        <a href="" class="linkHeader"><h1 id="logo">H.E.</h1></a>
-        <div id="loginAndBagWrapper">
-            <h5 class="linkHeader">ENTRAR</h5>
-            <a href="" class="linkHeader">
+        <!-- <a href="#contacts" class="headerLink"><h5>CONTATOS</h5></a> -->
+        <a href="#" class="headerLink"><h1 id="logo">H.E.</h1></a>
+        <!-- <div id="loginAndBagWrapper">
+            <h5 class="headerLink">ENTRAR</h5>
+            <a href="" class="headerLink">
                 <div class="bagWrapper">
                     <svg width="19" height="20" viewBox="0 0 19 20" fill="none" xmlns="http://www.w3.org/2000/svg"><path class="bagPath" d="M5.5 8.5C5.5 2.5 6.26293 0.5 9.5 0.5C12.7371 0.5 13.5 2.5 13.5 8.5M9.5 5H1.5L1 19H9.5H18L17.5 5H9.5Z" stroke="black" stroke-linejoin="round"/></svg>
                     <div class="bagAmount">(0)</div>
                 </div>
             </a>
-        </div>
+        </div> -->
     </header>
     <nav>
         <div id="navOptions">
-            <a href="<?= base_url('/'); ?>" class="linkHeader"><h5>INÍCIO</h5></a>
-            <a href="" class="linkHeader"><h5>SHOP</h5></a>
-            <a href="" class="linkHeader"><h5>CONTATOS</h5></a>
+            <a href="<?= current_url() == base_url() . 'index.php/' ? '#top' : base_url('/'); ?>" class="headerLink"><h5>INÍCIO</h5></a>
+            <a href="" class="headerLink"><h5>SHOP</h5></a>
+            <a href="#contacts" class="headerLink"><h5>CONTATOS</h5></a>
             <h5 id="">ENTRAR</h5>
-            <a href="" class="linkHeader">
+            <a href="" class="headerLink">
                 <div class="bagWrapper">
                     <svg width="19" height="20" viewBox="0 0 19 20" fill="none" xmlns="http://www.w3.org/2000/svg"><path class="bagPath" d="M5.5 8.5C5.5 2.5 6.26293 0.5 9.5 0.5C12.7371 0.5 13.5 2.5 13.5 8.5M9.5 5H1.5L1 19H9.5H18L17.5 5H9.5Z" stroke="black" stroke-linejoin="round"/></svg>
                     <div class="bagAmount">(0)</div>
                 </div>
             </a>
         </div>
-        <a href="<?= base_url('/'); ?>" class="linkHeader"><h2 id="miniLogo">H.E.</h3></a>
+        <a href="<?= current_url() == base_url() . 'index.php/' ? '#top' : base_url('/'); ?>" class="headerLink"><h2 id="miniLogo">H.E.</h3></a>
     </nav>
